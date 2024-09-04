67c6a44d5db4aac4262946931ea5bd32f4fbdd3c
SH_kim <dal2018@naver.com>
2024-09-04 19:48:07 +0900

    produts/views.py @api_view 상품조회

--- products/views.py
+++ products/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+from .models import Product
+from django.core import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProductSerializer 
+from rest_framework import status
+
 
 # Create your views here.
 @api_view(["GET"])
 def product_list(request):
-    pass
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
