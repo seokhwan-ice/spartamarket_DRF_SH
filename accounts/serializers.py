79713ca6aeda099bec2dae5298351cb323e71883
SH_kim <dal2018@naver.com>
2024-09-06 11:41:08 +0900

    시이얼라이저에 회원가입시 필수사항 정의

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,3 +5,11 @@ class UserSignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+        extra_kwargs = {
+            'password': {'write_only': True},  # write_only: 필드를 작성할 때만 사용,응답에서 제외
+            'email': {'required': True},  # required': True : 필드 필수! 요청시 반드시 제공해어야한다
+            'username': {'required': True},   
+            'first_name': {'required': True},  
+            'nickname': {'required': True}, 
+            'birthday': {'required': True}  
+        }
